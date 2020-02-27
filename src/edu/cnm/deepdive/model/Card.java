564fd03e626e091ee73a4f2ed34fbf18a8af2c96
Jose Ortiz <jose.ortiz@live.com>
2020-02-27 10:42:37 -0700

    Card implements Comparable.

--- src/edu/cnm/deepdive/model/Card.java
+++ src/edu/cnm/deepdive/model/Card.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.model;
 
+import java.util.Comparator;
 import java.util.Objects;
 
 /**
@@ -8,7 +9,7 @@ import java.util.Objects;
  *
  * @author Jortiz07.
  */
-public class Card {
+public class Card implements  Comparable<Card> {
 
   private final Suit suit;
   private final Rank rank;
@@ -62,5 +63,10 @@ public class Card {
       }
     }
     return comparison;
+  }
+
+  @Override
+  public int compareTo(Card other) {
+    return Comparator.comparing(Card::getSuit).thenComparing(Card::getRank).compare(this, other);
   }
 }
